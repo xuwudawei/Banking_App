57b9fc7974b486859eb25d856d9f70d4d9089635
xuwudawei <xuwudawei@outlook.com>
2020-08-23 02:40:22 +0530

    done

--- banking_app/lib/landingPage.dart
+++ banking_app/lib/landingPage.dart
@@ -3,41 +3,36 @@ import 'package:flutter/material.dart';
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: new BoxDecoration(
-          image: new DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage(
-              'assets/homepagepic.jpg',
-            ),
+    return Container(
+      decoration: new BoxDecoration(
+        image: new DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage(
+            'assets/homepagepic.jpg',
           ),
         ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.black54,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(left: 25, top: 50),
-                child: Image.asset('assets/davidlogo.png'),
-                width: 140,
-                height: 300,
-              ),
-              SizedBox(
-                height: 90,
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 25, top: 50),
-                child: Stack(
-                  children: <Widget>[Text('hiref')],
-                ),
-              )
-            ],
-          ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black54,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 25, top: 50),
+              child: Image.asset('assets/davidlogo.png'),
+              width: 140,
+              height: 300,
+            ),
+            SizedBox(
+              height: 90,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 25, top: 50),
+            )
+          ],
         ),
       ),
     );
